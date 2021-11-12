1981c5bc486f16343f2cb3d43efa7236ef1e8b33
veluminous <sungyoon606@naver.com>
2021-11-13 04:42:40 +0900

    Change environmet variable settings

--- tasks.py
+++ tasks.py
@@ -9,15 +9,16 @@ from scrapy.utils.project import get_project_settings
 from multiprocessing import Process
 
 from beach_forecast_scraper.spiders.forecast_spider import ForecastSpider
+from config import BROKER_SERVER
 
 
 sys.path.append(os.path.dirname(__file__))
-app = Celery("tasks", backend="rpc://", broker="amqp://guest:guest@localhost:5672//")
+app = Celery("tasks", backend="rpc://", broker=BROKER_SERVER)
 
 app.conf.beat_schedule = {
     'run_scraper_task-every-hour': {
         'task': 'tasks.run_scraper_task',
-        'schedule': crontab(minute=0),
+        'schedule': crontab(),
     },
 }
 app.conf.timezone = 'Asia/Seoul'
