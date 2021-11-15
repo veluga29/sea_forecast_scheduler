1a2afe26e9d98e6127a5a5a1e27474562acba3b3
veluminous <sungyoon606@naver.com>
2021-11-15 11:57:18 +0900

    Fix tasks.py

--- tasks.py
+++ tasks.py
@@ -13,7 +13,7 @@ from beach_forecast_scraper.spiders.forecast_spider import ForecastSpider
 from config import BROKER_SERVER
 
 # Add current path for windows
-# sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(__file__))
 
 app = Celery("tasks", broker=BROKER_SERVER)
 
